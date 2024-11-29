54fa9d9b92669c1c2ca3c88139c3ed5069e0db5e
Joshua Durham <jdurham38@gmail.com>
2024-11-28 20:19:54 -0500

    update api files

--- pages/api/check-username.ts
+++ pages/api/check-username.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import { profanity } from '@2toad/profanity';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
